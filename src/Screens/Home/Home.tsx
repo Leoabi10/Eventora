2f41b97995b54db4962fe9abde21aa48348d0e50
Leoabi10 <abiashwin143@gmail.com>
2025-09-10 20:35:51 +0530

    Integrated the tab switching logic and redesigned the home screen

--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -1,62 +1,91 @@
 import React from "react";
-import { FlatList, Image, ImageBackground, Text, TouchableOpacity, View } from "react-native";
+import { Dimensions, FlatList, Image, ImageBackground, StatusBar, Text, TouchableOpacity, View } from "react-native";
 import MyComponent from "../../Components/bottomNavigation";
 import { useNavigation } from "@react-navigation/native";
 import Icon from 'react-native-vector-icons/FontAwesome';
-import {
-    createDrawerNavigator,
-    DrawerContentScrollView,
-    DrawerItemList,
-    DrawerItem,
-  } from '@react-navigation/drawer';
-import type { DrawerNavigationProp } from "@react-navigation/drawer";
+import LinearGradient from "react-native-linear-gradient";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 type RootDrawerParamList = {
     Home: undefined;
     User: undefined;
-  };
+};
 
 const Home = () => {
 
+    const { width, height } = Dimensions.get("window");
+    const insets = useSafeAreaInsets();
     const navigation = useNavigation()
-    const users = [{uri: require("../../../Assets/man2.jpg"),Name: "Leo", mail: "leo@gmail.com"},
-        {uri: require("../../../Assets/man.jpg"),Name: "Andres", mail: "andres@gmail.com"},
-        {uri: require("../../../Assets/man3.jpg"),Name: "Thiago", mail: "thiago@gmail.com"},
-        {uri: require("../../../Assets/man3.jpg"),Name: "Thiago", mail: "thiago@gmail.com"},
-        {uri: require("../../../Assets/man2.jpg"),Name: "Leo", mail: "leo@gmail.com"}
+    const users = [{ uri: require("../../../Assets/man2.jpg"), Name: "Leo", mail: "leo@gmail.com" },
+    { uri: require("../../../Assets/man.jpg"), Name: "Andres", mail: "andres@gmail.com" },
+    { uri: require("../../../Assets/man3.jpg"), Name: "Thiago", mail: "thiago@gmail.com" },
+    { uri: require("../../../Assets/man3.jpg"), Name: "Thiago", mail: "thiago@gmail.com" },
+    { uri: require("../../../Assets/man2.jpg"), Name: "Leo", mail: "leo@gmail.com" }
     ]
     const eventImage = require("../../../Assets/Event_image.jpg")
-    return(
-        <View style={{flex: 1}}>
-            <View style={{ height: "25%"}}>
-                <ImageBackground source={eventImage} resizeMode="cover" style={{width: "100%", height: "100%", alignItems: "center", justifyContent: "space-evenly"}}>
-                    <TouchableOpacity style={{position: "absolute", left:10, top:10}} onPress={() => navigation.openDrawer()}>
-                        <Icon name="bars" size={25} color="#fff" />  
-                    </TouchableOpacity> 
-                    <Image source={require("../../../Assets/Event_car.png")} style={{width: 150, height: 150}}/>
-                    <Text style={{ color: "white", fontSize: 25, fontWeight: "bold"}}>Welcome Beauties!</Text>
-                </ImageBackground>
-            </View>
-            <View style={{alignItems: "center", justifyContent: "center", backgroundColor: "#F0F0F0", flex: 1}}>
-                <FlatList
-                data={users}
-                renderItem={({item, index}) => (
-                    <TouchableOpacity style={{ marginTop: 15, marginBottom: 15, width: "100%", borderRadius: 10, alignItems: "center", justifyContent: "space-evenly", flexDirection: "row", backgroundColor: "#598565", padding: 30}} key={index} onPress={() => navigation.navigate("User")}> 
-                    <View>
-                        <Image source={item.uri} style={{height: 120, width: 120, borderRadius: 70}}/>
+    return (
+        <View style={{ flex: 1 }}>
+            <StatusBar barStyle="light-content" backgroundColor="transparent" translucent={true} />
+            <LinearGradient
+                colors={['#000000', '#4A6CF7', '#7B2FF7']}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 1 }}
+                style={{
+                    height: insets.top,
+                    width: "100%",
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    backgroundColor: "red"
+                }}
+            />
+            <ImageBackground
+                source={require("../../../Assets/Images/home's_background.png")}
+                style={{
+                    width: "100%",
+                    height: "52%",
+
+                }}
+            >
+
+                <View style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "flex-start",
+                    paddingTop: 45,
+                }}>
+                    <TouchableOpacity onPress={() => navigation.openDrawer()}>
+                        <Image
+                            source={require("../../../Assets/icons/drawer_menu.png")}
+                            style={{ width: 35, height: 35, left: 10 }}
+                        />
+                    </TouchableOpacity>
+
+
+                    <View style={{ alignItems: "center" }}>
+                        <View style={{ flexDirection: "row", alignItems: "center", gap: 3 }}>
+                            <Text style={{ color: "white", fontSize: 15, opacity: 0.7 }}>
+                                Current Location
+                            </Text>
+                            <TouchableOpacity>
+                                <Icon name="chevron-down" size={13} color="#fff" />
+                            </TouchableOpacity>
+                        </View>
+                        <Text style={{ color: "white", fontSize: 13 }}>
+                            India, Tamil Nadu
+                        </Text>
                     </View>
-                    <View>
-                        <Text>Name : {item.Name}</Text>
-                        <Text>Mail : {item.mail}</Text>
-                    </View>    
-                </TouchableOpacity>
-                )}
-                initialNumToRender={3}
-                maxToRenderPerBatch={3}
-                windowSize={5}
-                showsVerticalScrollIndicator={false}
-                />
-            </View>
+
+
+                    <TouchableOpacity onPress={() => navigation.openDrawer()} style={{right: 15, top: 6}}>
+                        <Icon name="bell" size={23} color="#fff" />
+                    </TouchableOpacity>
+                </View>
+                <View>
+
+                </View>
+            </ImageBackground>
+
         </View>
     )
 }
--- src/Screens/User/User.tsx
+++ src/Screens/User/User.tsx
@@ -1,90 +1,107 @@
 import React, { useState } from "react";
-import { Image, Text, View, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
+import {
+    Image,
+    Text,
+    View,
+    StyleSheet,
+    useWindowDimensions,
+    TouchableOpacity,
+} from "react-native";
+import LinearGradient from "react-native-linear-gradient";
+import { TabView, SceneMap, TabBar } from "react-native-tab-view";
 
 const User = () => {
-    const [tab, setTab] = useState<'Description' | 'Address'>('Description');
+    const layout = useWindowDimensions();
+    const [index, setIndex] = useState(0);
+
+    const renderScene = SceneMap({
+        first: Description,
+        second: Address,
+    });
+
+    const routes = [
+        { key: "first", title: "Description" },
+        { key: "second", title: "Address" },
+    ];
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
-                <Image source={require("../../../Assets/Private_party.jpg")} style={styles.headerImage} />
+                <Image
+                    source={require("../../../Assets/Private_party.jpg")}
+                    style={styles.headerImage}
+                />
                 <Text style={styles.titleText}>Private party</Text>
-                <View style={styles.dateTimeContainer}><Text style={styles.dateTimeText}>Date: 02/Sep</Text><Text style={styles.dateTimeText}>Time: 05:00PM</Text></View>
-            </View>
-            <View>
-                <View style={{ alignItems: "center", justifyContent: "center" }}>
-                    <View style={{ flexDirection: "row", justifyContent: "space-evenly", width: "70%", marginBottom: 20, marginTop: 20 }}>
-                        <Tab label="Description" active={tab === 'Description'} onPress={() => setTab("Description")} />
-                        <Tab label="Address" active={tab === 'Address'} onPress={() => setTab("Address")} />
-                    </View>
+                <View style={styles.dateTimeContainer}>
+                    <Text style={styles.dateTimeText}>Date: 02/Sep</Text>
+                    <Text style={styles.dateTimeText}>Time: 05:00PM</Text>
                 </View>
-                {tab === "Description" && (
-                    <>
-                        <Text style={{ textAlign: "center" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            The primary purpose of using lorem ipsum in React Native development is to:
-                            Visualize UI layouts:
-                            See how content will flow and fit within the designed components without waiting for real data.
-                            Test styling and responsiveness:
-                            Ensure that text truncation, line breaks, and other styling elements behave as expected across different screen sizes and orientations.
-                            Facilitate parallel development:
-                            Allow frontend development to proceed independently while backend APIs or content creation are still in progress.
-                            Should you find this library beneficial, kindly contemplate the option of sponsoring. Our envisioned endeavors encompass the restructuring of the repository into a monorepo architecture. This transition will empower independent versioning of icon sets, enhance performance, reduce bundle size, and simplify community contributions. Your sponsorship plays a pivotal role in materializing these advancements.
-                        </Text>
-                        <Image source={require("../../../Assets/design_img.png")} style={{ width: "100%", height: 200, alignItems: "center", justifyContent: "center" }} />
-                    </>
-                )}
-                {tab === "Address" && (
-                    <>  
-                        <View style={{gap: 10}}>
-                            <Text style={{ textAlign: "center", fontSize: 20, fontWeight: "bold", color: "#b50427" }}>Short and sweet
-                            </Text>
-                            <Text style={{ textAlign: "center", fontWeight: "bold", }}>Short and sweet is located at T. Kodimangalam,</Text>
-                            <Text style={{ textAlign: "center", fontWeight: "bold", }}>Thirumal Puram Post, Madurai, Tamil Nadu 625014</Text>
-                        </View>
-                    </>
-                )}
             </View>
+
+            <TabView
+                navigationState={{ index, routes }}
+                renderScene={renderScene}
+                onIndexChange={setIndex}
+                initialLayout={{ width: layout.width }}
+                swipeEnabled
+                renderTabBar={(props) => (
+                    <TabBar
+                        {...props}
+                        indicatorStyle={{ backgroundColor: "transparent" }}
+                        style={{
+                            backgroundColor: "transparent",
+                            elevation: 0,
+                        }}
+                        renderTabBarItem={({ route, focused, onPress, onLongPress }) => {
+                            const isFocused =
+                                typeof focused === "boolean"
+                                    ? focused
+                                    : props.navigationState.index ===
+                                    props.navigationState.routes.findIndex((r) => r.key === route.key);
+
+                            return (
+                                <TouchableOpacity
+                                    key={route.key}
+                                    onPress={onPress}
+                                    onLongPress={onLongPress}
+                                    accessibilityRole="button"
+                                    accessibilityState={isFocused ? { selected: true } : {}}
+                                    style={{ marginHorizontal: 20 }}
+                                    activeOpacity={0.85}
+                                >
+                                    <LinearGradient
+                                        colors={
+                                            isFocused
+                                                ? ["#4A6CF7", "#7B2FF7"]
+                                                : ["#EDEFF2", "#E0E4F8"]
+                                        }
+                                        start={{ x: 0, y: 0 }}
+                                        end={{ x: 1, y: 0 }}
+                                        style={{
+                                            paddingVertical: 12,
+                                            paddingHorizontal: 55,
+                                            borderRadius: 8,
+                                            alignItems: "center",
+                                            justifyContent: "center",
+                                        }}
+                                    >
+                                        <Text
+                                            style={{
+                                                color: isFocused ? "white" : "#333",
+                                                fontWeight: "600",
+                                            }}
+                                        >
+                                            {route.title}
+                                        </Text>
+                                    </LinearGradient>
+                                </TouchableOpacity>
+                            );
+                        }}
+                    />
+                )}
+                style={{ flex: 1, top: 20 }}
+            />
         </View>
-    )
-}
-
-interface TabProps {
-    label: string;
-    onPress: () => void;
-    active?: boolean;
-}
-
-const Tab: React.FC<TabProps> = ({ label, onPress, active = false }) => {
-    return (
-        <TouchableOpacity
-            style={{
-                opacity: active ? 1 : 0.5,
-                backgroundColor: "#345cc2",
-                borderRadius: 5,
-                padding: 10
-            }}
-            onPress={onPress}>
-            <TouchableOpacity
-                onPress={onPress}
-                style={{
-                    backgroundColor: 'transparent',
-                    width: 85,
-                    height: 33,
-                    borderRadius: 40,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                }}>
-                <Text
-                    style={{
-                        fontSize: 14,
-                        textAlign: 'center',
-                        fontFamily: 'roboto',
-                        color: 'white',
-                        fontWeight: '700',
-                    }}>
-                    {label}
-                </Text>
-            </TouchableOpacity>
-        </TouchableOpacity>
     );
 };
 
@@ -92,35 +109,67 @@ export default User;
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
     },
     header: {
         height: "35%",
         alignItems: "center",
-        gap: 13
+        gap: 13,
     },
-    headerImage:
-    {
+    headerImage: {
         width: "100%",
-        height: "60%"
+        height: "60%",
     },
-    titleText:
-    {
+    titleText: {
         fontSize: 35,
-        color: '#b50427',
-        fontWeight: "bold"
+        color: "#b50427",
+        fontWeight: "bold",
     },
-    dateTimeContainer:
-    {
+    dateTimeContainer: {
         flexDirection: "row",
         justifyContent: "space-evenly",
         width: "100%",
     },
     dateTimeText: {
         fontSize: 15,
-        fontWeight: 600,
+        fontWeight: "600",
         backgroundColor: "#d9c7b6",
         borderRadius: 6,
-        padding: 10
-    }
+        padding: 10,
+    },
 });
+
+const Description = () => (
+    <View style={{ padding: 16 }}>
+        <Text style={{ textAlign: "center", color: "black" }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua...
+        </Text>
+        <Image
+            source={require("../../../Assets/design_img.png")}
+            style={{ width: "100%", height: 200, marginTop: 20 }}
+            resizeMode="contain"
+        />
+    </View>
+);
+
+const Address = () => (
+    <View style={{ padding: 16, gap: 10 }}>
+        <Text
+            style={{
+                textAlign: "center",
+                fontSize: 20,
+                fontWeight: "bold",
+                color: "#b50427",
+            }}
+        >
+            Short and sweet
+        </Text>
+        <Text style={{ textAlign: "center", fontWeight: "bold" }}>
+            Short and sweet is located at T. Kodimangalam,
+        </Text>
+        <Text style={{ textAlign: "center", fontWeight: "bold" }}>
+            Thirumal Puram Post, Madurai, Tamil Nadu 625014
+        </Text>
+    </View>
+);
